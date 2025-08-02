0c712301711709f2eeb7ff1661963d2a9b45f616
HannanehSharifi <hannaneh.sharifii@gmail.com>
2025-08-02 14:12:29 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function calculate(operator) {
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let result;
+
+    if (isNaN(num1) || isNaN(num2)) {
+        result = "please enter number!";
+    }
+    else {
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num2 !== 0 ? num1 / num2 : "not possible!";
+                break;
+        }
+    }
+    document.getElementById("result").textContent = "result:" + result;
+}
